293825a1e3d6ae4b2f71c0cd36102465c29757e3
johnpauljpc <jpcwork081@gmail.com>
2023-06-02 20:59:57 +0100

    wrote some tests for Todo Model

--- Todo/backend/todos/tests.py
+++ Todo/backend/todos/tests.py
@@ -1,4 +1,20 @@
 from django.test import TestCase
-
+from .models import Todo
 
 # Create your tests here.
+class TestTodoModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='code today', body = 'just testing the model, so chill', completed = False)
+
+    def test_title(self):
+        todo = Todo.objects.get(id=1)
+        expects = 'code today'
+        self.assertEqual(todo.title, expects)
+        print('No Error1')
+
+    def test_body(self):
+        todo = Todo.objects.get(id=1)
+        expects = 'just testing the model, so chill'
+        self.assertEqual(todo.body, expects)
+        print('No Error2')
