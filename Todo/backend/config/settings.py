28a5e7bc37888bb149d7ba9a2af0b011da7009c7
johnpauljpc <jpcwork081@gmail.com>
2023-06-02 20:44:05 +0100

    API for todo built

--- Todo/backend/config/settings.py
+++ Todo/backend/config/settings.py
@@ -38,18 +38,26 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'todos',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware' #for CORS HEADERS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+#domains/Ports that can access this API
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+)
 
 ROOT_URLCONF = 'config.urls'
 
