7060b92f5ab93050ef45c465033d4d4d007500b5
johnpauljpc <jpcwork081@gmail.com>
2023-06-08 10:57:19 +0100

    added login and logout to api

--- blogapi/config/urls.py
+++ blogapi/config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# import rest_framework
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('blog.urls')),
 ]
