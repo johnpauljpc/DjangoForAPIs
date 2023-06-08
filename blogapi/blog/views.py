6dc7837b4324eea2dd90b96c28b182b782725bbf
johnpauljpc <jpcwork081@gmail.com>
2023-06-08 20:33:23 +0100

    wrote a custom permission - 'IsAuthorOrReadOnly' for blogapi

--- blogapi/blog/views.py
+++ blogapi/blog/views.py
@@ -3,15 +3,16 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework.generics import  ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class ListCreateAPI(ListCreateAPIView):
     # model = Post
-    permission_classes = [IsAuthenticatedOrReadOnly ]
+    permission_classes = [IsAuthenticatedOrReadOnly ] #permission at view level, it ovverides that of the project level
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class RUD(RetrieveUpdateDestroyAPIView):
-    # permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
