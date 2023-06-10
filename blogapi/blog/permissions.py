28594d2dcbf761604bd7da76fae1385579ef6f48
johnpauljpc <jpcwork081@gmail.com>
2023-06-10 12:16:10 +0100

    .

--- blogapi/blog/permissions.py
+++ blogapi/blog/permissions.py
@@ -10,7 +10,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         # Write permissions are only allowed to the author of a post
-        return obj.author == request.user
+        return obj.author == request.user 
             
      
 
