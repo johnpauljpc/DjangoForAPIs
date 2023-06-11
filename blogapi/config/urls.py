d7935516a3cbb8fd725c349aca2ab8d67c507c8d
johnpauljpc <jpcwork081@gmail.com>
2023-06-11 18:36:35 +0100

    added third-party auth pakages

--- blogapi/config/urls.py
+++ blogapi/config/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('blog.urls')),
+    path('rest-auth/', include('dj_rest_auth.urls')),
+    path('allauth/', include('allauth.urls'))
 ]
