9981bed95f997d98ff194194ae3af1cbb542a87b
johnpauljpc <jpcwork081@gmail.com>
2023-06-14 18:07:04 +0100

    replaced paths with routers

--- blogapi/blog/urls.py
+++ blogapi/blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from .views import *
+from rest_framework.routers import SimpleRouter
+from .views import UsersViewset, PostViewset
 
-urlpatterns = [
-    path('', ListCreateAPI.as_view(), name='list-create'),
-    path('<int:pk>/', RUD.as_view(), name='cud'),
-    path('users/', UsersList.as_view(), name='user-list'),
-    path('user/<pk>/', UserDetail.as_view(), name='user-details')
-]
+
+router = SimpleRouter()
+router.register('users', UsersViewset, basename="users")
+router.register('', PostViewset, basename="post")
+
+urlpatterns = router.urls
