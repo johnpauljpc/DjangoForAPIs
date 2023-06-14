7d663301192441d3af612e8276f7d14840a41348
johnpauljpc <jpcwork081@gmail.com>
2023-06-14 19:22:28 +0100

    customized a permission (IsUserOrReadOnly)

--- blogapi/blog/permissions.py
+++ blogapi/blog/permissions.py
@@ -12,6 +12,20 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         # Write permissions are only allowed to the author of a post
         if request.user.is_authenticated:
             return obj.author == request.user
+        
+
+class IsUserOrReadOnly(permissions.BasePermission):
+    """
+    The request is authenticated as a user, or is a read-only request.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the author of a post
+        if request.user.is_authenticated:
+            return obj.id == request.user.id
             
      
 
--- blogapi/blog/views.py
+++ blogapi/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Post
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth import get_user_model
 from .serializers import PostSerializer, UserSerializer
-from .permissions import IsAuthorOrReadOnly
+from .permissions import IsAuthorOrReadOnly, IsUserOrReadOnly
 
 # Create your views here.
 class PostViewset(viewsets.ModelViewSet):
@@ -12,7 +13,7 @@ class PostViewset(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
 class UsersViewset(viewsets.ModelViewSet):
-    permission_classes = (IsAuthorOrReadOnly, )
+    permission_classes = ( IsUserOrReadOnly, )
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
