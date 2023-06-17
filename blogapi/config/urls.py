eeaaece5cab29db5cad68da2cd22edc29062853a
johnpauljpc <jpcwork081@gmail.com>
2023-06-17 21:43:48 +0100

    implemented documentation using a third-party package (drf-yasg)

--- blogapi/config/urls.py
+++ blogapi/config/urls.py
@@ -5,6 +5,26 @@ from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
 
+from drf_yasg.views import get_schema_view as gsv
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = gsv( # new
+openapi.Info(
+title="Blog API",
+default_version="v1",
+description="A sample API for learning DRF",
+terms_of_service="https://www.google.com/policies/terms/",
+contact=openapi.Contact(email="contact@blogapi.com"),
+license=openapi.License(name="BSD License"),
+),
+public=True,
+permission_classes=(permissions.AllowAny,),
+)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('blog.urls')),
@@ -24,8 +44,14 @@ urlpatterns = [
     ), name='swagger-ui'),
 
     #REDOC
-    path('redoc/', TemplateView.as_view(
+    path('re-doc/', TemplateView.as_view(
         template_name='redoc.html',
         extra_context={'schema_url':'openapi-schema'}
     ), name='redoc'),
+
+    # Endpoints as a result of drf_yasg
+    path('swagger/', schema_view.with_ui( # new
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
 ]
