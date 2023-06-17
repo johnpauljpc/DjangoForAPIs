d97c7cf051ba731f01a514fa2cef182b9a03122c
johnpauljpc <jpcwork081@gmail.com>
2023-06-17 19:09:28 +0100

    endpoint to generate dynamic schema implemented

--- blogapi/config/urls.py
+++ blogapi/config/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 # import rest_framework
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('dj_rest_auth.urls')),
     path('allauth/', include('allauth.urls')),
-    path('rest-auth/register/', include('dj_rest_auth.registration.urls'))
+    path('rest-auth/register/', include('dj_rest_auth.registration.urls')),
+    path('openapi', get_schema_view(
+        title="Blog API",
+        description="A sample API for learning DRF",
+        version="1.0.0"
+    ), name="openapi-schema"),
 ]
